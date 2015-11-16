9aa15cffc552501c55392d94c0f1d9a5300e5107
elc1798 <subliminalmau5@gmail.com>
2015-11-16 11:44:25 -0500

    Update

--- USACO/2012-nov/bronze/find/FindCow.java
+++ USACO/2012-nov/bronze/find/FindCow.java
@@ -5,49 +5,47 @@ import java.io.IOException;
 
 public class FindCow {
 
-	private static Scanner fin = null;
-	private static PrintWriter fout = null;
-	private static String line = "";
-	private static int[] hindLegs;
-	private static int[] frontLegs;
-	private static int numhind = 0;
-	private static int numfront = 0;
+    private static Scanner fin = null;
+    private static PrintWriter fout = null;
+    private static String line = "";
 
-	public static void readFile() throws IOException {
-		fin = new Scanner(new File("cowfind.in"));
-		line = fin.nextLine();
-		fin = null;
-	}
+    public static void readFile() throws IOException {
+        fin = new Scanner(new File("cowfind.in"));
+        line = fin.nextLine();
+        fin = null;
+    }
 
-	public static void outFile() throws IOException {
-		fout = new PrintWriter(new File("cowfind.out"));
-		String outstream = "" + getAns();
-		fout.println(outstream);
-		fout.close();
-		fout = null;
-	}
+    public static void outFile() throws IOException {
+        fout = new PrintWriter(new File("cowfind.out"));
+        String outstream = "" + getAns();
+        fout.println(outstream);
+        fout.close();
+        fout = null;
+    }
 
-	public static int getAns() {
-		int toReturn = 0;
-		for (int i = 0; i < line.length() - 1; i++) {
-			if (line.substring(i , i + 2).equals("((")) {
-				hindLegs[numhind] = i;
-				numhind++;
-			}
-			if (line.substring(i , i + 2).equals("))")) {
-				frontLegs[numfront] = i;
-				toReturn += numhind;
-				numfront++;
-			}
-		}
-		return toReturn;
-	}
+    public static int getAns() {
+        int toReturn = 0;
+        boolean notSearched = true;
+        int numClosed = 0;
+        for (int i = 0; i < line.length() - 1; i++) {
+            if (line.substring(i, i+2).equals("((")) {
+                if (notSearched) {
+                    for (int k = i + 1; k < line.length() - 1; k++) {
+                        if (line.substring(k, k+2).equals("))")) numClosed++;
+                    }
+                    notSearched = false;
+                }
+                toReturn += numClosed;
+            } else if (line.substring(i, i+2).equals("))") && !notSearched) {
+                numClosed--;
+            }
+        }
+        return toReturn;
+    }
 
-	public static void main(String[] args) throws IOException {
-		readFile();
-		hindLegs = new int[line.length()];
-		frontLegs = new int[line.length()];
-		outFile();
-	}
+    public static void main(String[] args) throws IOException {
+        readFile();
+        outFile();
+    }
 
 }
